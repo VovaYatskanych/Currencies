9cb6fcab3701226346e92367bb23c0cacd499e48
Volodymyr Yatskanych <volodymyr.yatskanych@itomy.ch>
2022-07-11 21:45:48 +0300

    Set up All currencies table view

--- Currencies/Flows/AllCurrencies/AllCurrenciesViewController.swift
+++ Currencies/Flows/AllCurrencies/AllCurrenciesViewController.swift
@@ -8,21 +8,50 @@
 import UIKit
 
 final class AllCurrenciesViewController: UIViewController {
-
+    private struct Constants {
+        static let cellID = "AllCell"
+        static let viewControllerID = "AllCurrencies"
+    }
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        view.backgroundColor = .yellow
+                
+        setupUI()
+    }
+    
+    private func setupUI() {
+        tableView.register(UINib(nibName: Constants.cellID, bundle: nil),
+                           forCellReuseIdentifier: Constants.cellID)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension AllCurrenciesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellID, for: indexPath) as? AllCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+
 extension AllCurrenciesViewController {
     private static var allCurrenciesStoryboard: UIStoryboard {
-        UIStoryboard(name: "AllCurrencies", bundle: .main)
+        UIStoryboard(name: Constants.viewControllerID, bundle: .main)
     }
     
     static func instantiateAllCurrenciesViewController() -> AllCurrenciesViewController {
-        let allCurrenciesViewController = allCurrenciesStoryboard.instantiateViewController(withIdentifier: "AllCurrencies") as! AllCurrenciesViewController
+        let allCurrenciesViewController = allCurrenciesStoryboard.instantiateViewController(withIdentifier: Constants.viewControllerID) as! AllCurrenciesViewController
         return allCurrenciesViewController
     }
 }
---
+++ Currencies/Flows/AllCurrencies/Views/AllCell.swift
@@ -0,0 +1,50 @@
+//
+//  AllCell.swift
+//  Currencies
+//
+//  Created by Volodymyr Yatskanych on 11.07.2022.
+//
+
+import UIKit
+
+class AllCell: UITableViewCell {
+    private struct Constants {
+        static let cornerRadius: CGFloat = 6
+        static let buttonAddTitle = "Add"
+        static let buttonRemoveTitle = "Remove"
+    }
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var addButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+
+    private func setupUI() {
+        titleLabel.text = "Test"
+        addButton.layer.cornerRadius = Constants.cornerRadius
+        configureAddButton(isSelected: true)
+    }
+    
+    private func configureAddButton(isSelected: Bool) {
+        if isSelected {
+            addButton.isSelected = false
+            addButton.setTitle(Constants.buttonAddTitle, for: .normal)
+            addButton.backgroundColor = .gray
+        } else {
+            addButton.isSelected = true
+            addButton.setTitle(Constants.buttonRemoveTitle, for: .normal)
+            addButton.backgroundColor = .lightGray
+        }
+    }
+}
+
+// MARK: - Actions
+
+private extension AllCell {
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        configureAddButton(isSelected: sender.isSelected)
+    }
+}
